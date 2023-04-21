040167dce992c7461f3d4b732ce782c5d67fb824
andylamax <andylamax@programmer.net>
2023-04-21 16:56:35 +0300

    Attempting an alternate way of publishing

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,30 +1,20 @@
 plugins {
     kotlin("multiplatform")
     id("tz.co.asoft.library")
-    id("io.codearte.nexus-staging")
-    signing
 }
 
+description = "A collections of interoperable functions to easy interoperabiliy with java"
+
 kotlin {
-    if (Targeting.JVM) jvm {
-        library()
-        withJava()
-    }
-
-    if (Targeting.JS) js(IR) {
-        library()
-    }
-
-    if (Targeting.WASM) wasm {
-        library()
-    }
-
+    if (Targeting.JVM) jvm { library() }
+    if (Targeting.JS) js(IR) { library() }
+    if (Targeting.WASM) wasm { library() }
     val osxTargets = if (Targeting.OSX) osxTargets() else listOf()
-//    val ndkTargets = if (Targeting.NDK) ndkTargets() else listOf()
+    val ndkTargets = if (Targeting.NDK) ndkTargets() else listOf()
     val linuxTargets = if (Targeting.LINUX) linuxTargets() else listOf()
     val mingwTargets = if (Targeting.MINGW) mingwTargets() else listOf()
 
-    val nativeTargets = osxTargets + /* ndkTargets + */ linuxTargets + mingwTargets
+    val nativeTargets = osxTargets + ndkTargets + linuxTargets + mingwTargets
 
     sourceSets {
         val commonMain by getting
@@ -53,8 +43,3 @@ kotlin {
         }
     }
 }
-
-aSoftOSSLibrary(
-    version = asoft.versions.root.get(),
-    description = "A collections of interoperable functions to easy interoperabiliy with java"
-)
